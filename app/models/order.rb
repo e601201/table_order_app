0efb22922a4f4adc8ca06cee50acfff8a744722b
e601201 <k.arthur1111@gmail.com>
2026-04-23 19:00:44 +0900

    オーダーモデルの修正 | statusの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
-  enum :status, { pending: 0 }, default: :pending
+  enum :status, { pending: 0, in_progress: 1, ready: 2, completed: 3 }, default: :pending
 
   validates :order_number, presence: true, uniqueness: true
   validates :table_number, :subtotal, :tax, :total, :placed_at, presence: true
